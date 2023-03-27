8a20ed01c956a6185f2de424c026face4a298bba
azilrababe <azilrababe@gmail.com>
2023-03-27 18:09:50 +0000

    Polishing ...

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,Response,url_for
+from flask import Flask,render_template,Response
 import tensorflow as tf
 import cv2
 import numpy as np
@@ -15,11 +15,11 @@ model=tf.keras.models.load_model('model.h5')
 
 # alarm_sound=False
 
+
 def generate_frames():
+
     camera=cv2.VideoCapture(0)
-
     while True:
-
         ## read the camera frame
         success,frame=camera.read()
         if not success:
@@ -32,13 +32,13 @@ def generate_frames():
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
              #Draw the rectangle around each face
             for (x, y, w, h) in faces:
-                cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
                 roi_gray = gray[y:y+h, x:x+w]
                 roi_color = frame[y:y+h, x:x+w]
                 eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 3)
                 for (ex, ey, ew, eh) in eyes:
                     # text = str(model.predict(frame[ey:ey+eh,ex:ex+ew]))
-                    cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+                    cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 0, 255), 2)
                     final_image = cv2.resize(roi_color, (224,224))
                     final_image = np.expand_dims (final_image,axis =0) ## need fourth dimension
                     final_image= final_image/255.0
@@ -51,11 +51,9 @@ def generate_frames():
                         # alarm_sound=True
                         # playsound('./alarm/alarm.wav')
 
-
-
                 origin = (x,y)
                 # text = drowsiness_model(frame,eyes,model)
-                cv2.putText(frame, text, origin , fontFace = cv2.FONT_HERSHEY_COMPLEX, fontScale = 1.5, color = (250,225,100))
+                cv2.putText(frame, text, origin , fontFace = cv2.FONT_HERSHEY_COMPLEX, fontScale = 2, color = (0,255,0))
                 ret, buffer = cv2.imencode('.jpg', frame)
 
                 if cv2.waitKey(100) == ord('q'):
@@ -65,6 +63,8 @@ def generate_frames():
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
+
+# Routes
 @app.route('/')
 def index():
     return render_template('feed.html')
